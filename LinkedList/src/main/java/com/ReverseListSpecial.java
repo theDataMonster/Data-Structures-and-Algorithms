5209a65ea2b2d50e5738ae2a11496bf7abeda2e5
Pratyush Dwivedi <pratyushdvd@gmail.com>
2020-02-06 17:08:03 +0530

    Special Linked List Problems added

--- LinkedList/src/main/java/com/ReverseListSpecial.java
+++ LinkedList/src/main/java/com/ReverseListSpecial.java
@@ -33,9 +33,49 @@ public class ReverseListSpecial {
 			j++;
 		}
 	}
+	
+	static Node push(Node head, int data) {
+		
+		Node new_node=new Node(data);
+		new_node.next= head;
+		head=new_node;
+		
+		return head;
+	}
+	
+	static int printList(Node head) {
+		
+		int i=0;
+		Node temp= head;
+		while(temp!=null) {
+			
+			System.out.print(temp.data+ " ");
+			temp=temp.next;
+			i++;
+			
+		}
+		
+		return i;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Node head=null;
+		
+		head=push(head,1);
+		head=push(head, 2);
+		head=push(head, 3);
+		head=push(head, 4);
+		head= push(head, 5);
+		head=push(head, 6);
+		
+		System.out.println("Given Linked List-->");
+		
+		int n=printList(head);
+		System.out.println();
+		System.out.println("Reversed List-->");
+		printReverse(head,n);
 
 	}
 
